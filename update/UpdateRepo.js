673dd529905944056df76e36a624067b40c70b7f
SuGueVar30 <susoguevar@gmail.com>
2024-07-14 17:42:01 -0500

    Actualización_01

---
+++ update/UpdateRepo.js
@@ -0,0 +1,23 @@
+import { execSync } from "child_process";
+import fs from "fs";
+
+const versionFilePath = "./Version.txt";
+let versionNumber = 0;
+
+if (fs.existsSync(versionFilePath)) {
+  versionNumber = parseInt(fs.readFileSync(versionFilePath, "utf8"), 10);
+}
+
+versionNumber += 1;
+fs.writeFileSync(versionFilePath, versionNumber.toString());
+const commitMessage = `Actualización_${versionNumber.toString().padStart(2, "0")}`;
+
+try {
+  execSync("git add .");
+  execSync(`git commit -m "${commitMessage}"`);
+  execSync("git pull origin main");
+  execSync("git push origin main");
+  console.log(`Repositorio actualizado con commit: ${commitMessage}`);
+} catch (error) {
+  console.error("Error al actualizar el repositorio:", error);
+}
